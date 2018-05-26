ffc385f332b82631588b962aa6b10e6e13c25fe4
Kundan Kale <kundan.kale@blancco.com>
2018-05-26 15:02:14 +0530

    second

--- src/main/java/com/mathlib/Add.java
+++ src/main/java/com/mathlib/Add.java
@@ -1,5 +1,14 @@
 package com.mathlib;
 
+import android.content.Context;
+import android.util.Log;
+
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+
 /**
  * Created by Kundan Kale on 26/5/18.
  * Blancco
@@ -7,10 +16,35 @@ package com.mathlib;
  */
 public class Add {
 
-    public Add(){
+    Context context;
+    AmazonS3 s3Client;
+    public Add(Context context){
 
+        this.context = context;
     }
     public int addNumbers(int a,int b){
         return a+b;
+    }
+    public void s3credentialsProvider(){
+
+        // Initialize the AWS Credential
+        Log.d("POC","Library method aws");
+        CognitoCachingCredentialsProvider cognitoCachingCredentialsProvider =
+                new CognitoCachingCredentialsProvider(
+                        context,
+                        "us-east-1:521c4556-b969-456d-93ec-a3fbc8bfe37f", // Identity Pool ID
+                        Regions.US_EAST_1 // Region
+                );
+        createAmazonS3Client(cognitoCachingCredentialsProvider);
+    }
+    public void createAmazonS3Client(CognitoCachingCredentialsProvider
+                                             credentialsProvider){
+
+        // Create an S3 client
+        Log.d("POC","Library method aws createAmazonS3Client");
+        s3Client = new AmazonS3Client(credentialsProvider);
+
+        // Set the region of your S3 bucket
+        s3Client.setRegion(Region.getRegion(Regions.US_EAST_1));
     }
 }
