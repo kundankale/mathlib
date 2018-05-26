1fb2318fd8fb18f9709aba4e82aeeceb4463b6bc
Kundan Kale <kundan.kale@blancco.com>
2018-05-26 12:03:27 +0530

    first commit

---
+++ src/main/java/com/mathlib/Add.java
@@ -0,0 +1,16 @@
+package com.mathlib;
+
+/**
+ * Created by Kundan Kale on 26/5/18.
+ * Blancco
+ * kundan.kale@blancco.com
+ */
+public class Add {
+
+    public Add(){
+
+    }
+    public int addNumbers(int a,int b){
+        return a+b;
+    }
+}
